f0aa4211628f722902887e06f7f2a1b2e9cea711
djimelm <djimelm@gmail.com>
2024-06-23 15:34:36 -0500

    credential checked correctly

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-Install Cypress
+Install Cypress and the CORS middleware:
 
 ```sh
 npm install cypress --save-dev
+npm install cors
 ```
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+const cors = require("cors");
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Import routes
-const userRoutes = require('./routes/users');
-const deviceRoutes = require('./routes/devices');
+const userRoutes = require("./routes/users");
+const deviceRoutes = require("./routes/devices");
 // Add more routes as needed
 
 // Use routes
-app.use('/users', userRoutes);
-app.use('/devices', deviceRoutes);
+app.use("/users", userRoutes);
+app.use("/devices", deviceRoutes);
 // Add more route uses as needed
 
 const port = process.env.PORT || 3000;
